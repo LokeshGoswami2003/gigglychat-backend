602562c4a20ad2ccdd802b21c822f55b8969d9c5
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2024-02-07 16:57:46 +0530

    adding middlewares

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(morgan("dev"));
+}
+
+app.use(helmet());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(mongoSanitize());
+app.use(cookieParser());
+app.use(compression());
+app.use(
+    fileUpload({
+        useTempFiles: true,
+    })
+);
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("hello from server");
+});
+
+export default app;
